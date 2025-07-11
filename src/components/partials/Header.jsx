626594ac4cf6f1b4d309faddb491dd19a2d23db1
Kyla <cpe.cruz.marklester@gmail.com>
2025-07-11 13:27:09 +0800

    commit

--- src/components/partials/Header.jsx
+++ src/components/partials/Header.jsx
@@ -1,23 +1,76 @@
 import React from 'react'
 import Logo from '../../svg/Logo';
+import { FaTimes } from 'react-icons/fa';
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+  
   return (
     <>
       {/* Header */}
-      <header className="bg-black text-white p-4 shadow-md">
+      <header className="bg-black text-white p-4 shadow-md sticky z-50">
         <div className="container mx-auto flex justify-between items-center">
           <Logo />
           <nav>
-            <ul className="flex space-x-6">
+            <ul className=" hidden md:flex space-x-6">
               <li className="hover:text-yellow-300 cursor-pointer">Home</li>
               <li className="hover:text-yellow-300 cursor-pointer">Races</li>
               <li className="hover:text-yellow-300 cursor-pointer">Drivers</li>
               <li className="hover:text-yellow-300 cursor-pointer">Teams</li>
             </ul>
+
+            <button
+              onClick={toggleMenu}
+              className="md:hidden text-xl focus:outline-none"
+            >
+              {isMenuOpen ? <FaTimes /> : "☰"}
+            </button>
           </nav>
         </div>
       </header>
+
+      {/* Mobile Menu */}
+      <div
+        className={`fixed inset-0 bg-black z-40 flex flex-col items-center justify-center transition-all duration-300 ease-in-out ${
+          isMenuOpen
+            ? "opacity-100 translate-y-0"
+            : "opacity-0 -translate-y-full pointer-events-none"
+        }`}
+      >
+
+        <nav>
+          <ul className="flex flex-col items-center space-y-8 text-white text-2xl">
+            <li
+              className="hover:text-yellow-300 cursor-pointer"
+              onClick={() => setIsMenuOpen(false)}
+            >
+              Home
+            </li>
+            <li
+              className="hover:text-yellow-300 cursor-pointer"
+              onClick={() => setIsMenuOpen(false)}
+            >
+              Races
+            </li>
+            <li
+              className="hover:text-yellow-300 cursor-pointer"
+              onClick={() => setIsMenuOpen(false)}
+            >
+              Drivers
+            </li>
+            <li
+              className="hover:text-yellow-300 cursor-pointer"
+              onClick={() => setIsMenuOpen(false)}
+            >
+              Teams
+            </li>
+          </ul>
+        </nav>
+      </div>
     </>
   );
 };
